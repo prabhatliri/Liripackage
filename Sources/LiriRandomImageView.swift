5e592f51a8b507abf482be94b6763c80e15b30ca
prabhat kasera <pkasera@netpace.com>
2022-05-20 15:08:51 +0530

    some more changes

--- Sources/LiriRandomImageView.swift
+++ Sources/LiriRandomImageView.swift
@@ -13,10 +13,17 @@ class LiriRandomImageView : UIView {
     let url = URL(string:"https://picsum.photos/200/300?random=10")!
     override init(frame: CGRect) {
         super.init(frame: frame)
-        let imageView = UIImageView(frame: self.bounds)
-        imageView.kf.setImage(with: url)
+        addKFView()
+    }
+    override class func awakeFromNib() {
+        super.awakeFromNib()
+        addKFView() 
     }
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    func addKFView() {
+        let imageView = UIImageView(frame: self.bounds)
+        imageView.kf.setImage(with: url)
     }
 }
